c58bb2f1e290f2b287e4eaea58ec87d57683016d
3T90hZ <165739979+3T90hZ@users.noreply.github.com>
2026-04-06 23:09:21 +0700

    Implement Ads

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -1,7 +1,6 @@
 using UnityEngine;
 using System.Collections.Generic;
 using TMPro;
-using UnityEngine.Apple.ReplayKit; // Thêm thư viện này để dùng TextMeshPro
 
 public class GameManager : MonoBehaviour
 {
